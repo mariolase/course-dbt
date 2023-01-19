7b13ab539b4e9d73baeb23293b0b5f73b0b12fdb
Mariola Sebastian <mariola.sebastian@brooklyndata.co>
2023-01-19 19:58:14 +0000

    added some models

---
+++ greenery/models/marts/core/dim_users.sql
@@ -0,0 +1,30 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+with users as (
+    select *
+    from {{ ref('stg_postgres__users') }}
+)
+
+, addresses as (
+    select * 
+    from {{ ref('stg_postgres__addresses') }}
+)
+
+
+select
+    users.first_name
+    , users.last_name
+    , users.email,
+    , users.created_at_utc::DATE AS created_date_utc
+    , addresses.address
+    , addresses.country
+    , addresses.state
+    , addresses.zipcode
+from  users
+left join addresses
+on users.address_guid = addresses.address_guid
+
